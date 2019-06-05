ef65c111d4ffba18ccd79515f8b26a5934d1767b
sudhanshu tiwari <sudhanshu@viosmedical.com>
2019-06-05 12:30:57 +0530

    Installed new package for logging logat

--- README.md
+++ README.md
@@ -20,3 +20,16 @@ app.listen(8000, () => {
 });
 ```
 Run the app -> ```node server.js```
+
+## Output
+```bash
+$ node server.js
+[INFO]  on  Wed, 05 Jun 2019 06:58:37 GMT  at  Server.app.listen (E:\Learning\projects\nodeapp\server.js:14:12) 
+Object0:  Example app listening on port 8000
+[DEBUG]  on  Wed, 05 Jun 2019 06:58:43 GMT  at  app.get (E:\Learning\projects\nodeapp\server.js:6:12) 
+Object0:  This is info
+[ERROR]  on  Wed, 05 Jun 2019 06:58:43 GMT  at  app.get (E:\Learning\projects\nodeapp\server.js:7:12) 
+Object0:  This is an error
+[WARN]  on  Wed, 05 Jun 2019 06:58:43 GMT  at  app.get (E:\Learning\projects\nodeapp\server.js:8:12) 
+Object0:  This is warning
+```
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+const logger = require('logat');
+
 app.get('/', (req, res) => {
-    console.log('unicorns & rainbows');
+    logger.debug('This is info');
+    logger.error('This is an error');
+    logger.warn('This is warning');
     res.send('Hello World!');
 
 });
-['log','warn','error'].forEach(a=>{let b=console[a];console[a]=(...c)=>{try{throw new Error}catch(d){b.apply(console,[d.stack.split('\n')[2].trim().substring(3).replace(__dirname,'').replace(/\s\(./,' at ').replace(/\)/,''),'\n',...c])}}});
+// ['log','warn','error'].forEach(a=>{let b=console[a];console[a]=(...c)=>{try{throw new Error}catch(d){b.apply(console,[d.stack.split('\n')[2].trim().substring(3).replace(__dirname,'').replace(/\s\(./,' at ').replace(/\)/,''),'\n',...c])}}});
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000');
+    logger.info('Example app listening on port 8000');
     
 });
